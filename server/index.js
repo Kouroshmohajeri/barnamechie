aa81ca47c15fed6c7128e71b7fbce0e19e236e74
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-11-15 14:41:58 +0100

    Port added

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/advertisements", advertisementRoutes);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8443;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
