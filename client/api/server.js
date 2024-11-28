a273dcd2bf050738bf0bda334635c8cedee712c1
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-11-28 01:07:03 +0100

    CORS updated

--- client/api/server.js
+++ client/api/server.js
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: process.env.BASE_URL || "https://barnamechie.es/api",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json", // Add other headers if needed
+  },
 });
 
 export default API;
