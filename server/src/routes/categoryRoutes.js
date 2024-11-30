a813cd42445f09d558c749e7a451ac055851671d
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-11-30 01:18:53 +0100

    removed authentication for categories

--- server/src/routes/categoryRoutes.js
+++ server/src/routes/categoryRoutes.js
@@ -5,23 +5,19 @@ import { authenticate, authorizeRoles } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 // Protect routes that require authentication
-router.use(authenticate);
+// router.use(authenticate);
 
 // Route to create a new category (only admin can create categories)
-router.post("/", authorizeRoles("admin"), CategoryController.createCategory);
+router.post("/", CategoryController.createCategory);
 
 // Route to get a category by its ID (accessible to any authenticated user)
 router.get("/:id", CategoryController.getCategoryById);
 
 // Route to update a category (only admin can update categories)
-router.put("/:id", authorizeRoles("admin"), CategoryController.updateCategory);
+router.put("/:id", CategoryController.updateCategory);
 
 // Route to delete a category (only admin can delete categories)
-router.delete(
-  "/:id",
-  authorizeRoles("admin"),
-  CategoryController.deleteCategory
-);
+router.delete("/:id", CategoryController.deleteCategory);
 
 // Route to get all categories (accessible to any authenticated user)
 router.get("/", CategoryController.getAllCategories);
