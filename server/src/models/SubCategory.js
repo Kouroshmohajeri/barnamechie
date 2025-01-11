6b9696129195c60915e9d3e23c93ba8f1e65783e
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2025-01-11 02:29:45 +0100

    testing

--- server/src/models/SubCategory.js
+++ server/src/models/SubCategory.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
-const subCategorySchema = new mongoose.Schema({
-  subcategoryId: { type: String, required: true, unique: true },
-  categoryId: { type: String, required: true }, // Relation to Category
-  name: { type: String, required: true },
-  isPaid: { type: Boolean, default: false }, // Indicates if the subcategory is paid
-});
+const subCategorySchema = new mongoose.Schema(
+  {
+    subcategoryId: { type: String, required: true, unique: true },
+    categoryId: { type: String, required: true }, // Relation to Category
+    name: { type: String, required: true },
+    isPaid: { type: Boolean, default: false }, // Indicates if the subcategory is paid
+  },
+  { collection: "subCategories" }
+);
 
 export default mongoose.model("SubCategory", subCategorySchema);
